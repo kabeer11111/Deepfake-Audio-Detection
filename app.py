efe12b1ba65724f6a3f940b4bbcd2206e8ace929
kabeer <140699551+kabeer11111@users.noreply.github.com>
2024-12-04 03:34:07 +0530

    app.py

--- app.py
+++ app.py
@@ -29,5 +29,6 @@ def predict():
         return render_template('result.html', prediction=prediction)
     return redirect(url_for('index'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
